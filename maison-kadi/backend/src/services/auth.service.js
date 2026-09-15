b3d8c7b80d74d5d33520212171616ebdf93ed908
rachida <fofanahawarachida@gmail.com>
2026-09-15 22:20:56 +0200

    feature: finalisation user (code only)

--- maison-kadi/backend/src/services/auth.service.js
+++ maison-kadi/backend/src/services/auth.service.js
@@ -2,8 +2,15 @@ const db = require('../config/db');
 
 exports.login = (email, password, callback) => {
   const sql = "SELECT * FROM users WHERE email = ? AND password = ?";
+
   db.query(sql, [email, password], (err, results) => {
-    if (err) return callback(err, null);
+
+    console.log("SQL ERROR :", err);
+    console.log("SQL RESULTS :", results);
+
+    if (err) {
+      return callback(err, null);
+    }
 
     if (results.length === 0) {
       return callback(null, { error: "Email ou mot de passe incorrect" });
